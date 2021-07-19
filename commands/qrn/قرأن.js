9c4c517ce44f47059770eb6f5057bbbe9bd16c38
Vexxo-Dev <75578911+Vexxo-Dev@users.noreply.github.com>
2021-07-19 14:25:28 +0200

    Update قرأن.js

--- commands/qrn/قرأن.js
+++ commands/qrn/قرأن.js
@@ -1,6 +1,6 @@
 module.exports = {
       name: "قرأن",
-      code: `$playSong[ https://www.youtube.com/watch?v=M6z0Qql4-qo;1m;لم يتم تشغيل القرأن بسبب بكج خربان]
+      code: `$playSong[ https://www.youtube.com/watch?v=M6z0Qql4-qo;1m;لم يتم تشغيل القرأن لعدم وجودك في روم صوتي]
 $clear[1]
 تم تشغيل القرأن`
 }
